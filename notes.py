98ca6000626efa19749d90af724442c28cad3f42
Panos Kounoudis <39673318+pkounoudis@users.noreply.github.com>
2025-05-28 17:43:19 +0300

    Update notes.py

--- notes.py
+++ notes.py
@@ -32,3 +32,11 @@ bias = 2
 output = (inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias)
 
 print(output)
+
+
+inputs = [1.0, 2.0, 3.0, 2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2.0
+
+# The function if we add on more weight and input
+output = (inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + inputs[3]*weights[3] + bias)
