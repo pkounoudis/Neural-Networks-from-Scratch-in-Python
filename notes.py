2714ab01466d76d63a2995462f74287f797826da
Panos Kounoudis <39673318+pkounoudis@users.noreply.github.com>
2025-05-28 17:11:53 +0300

    Update notes.py

--- notes.py
+++ notes.py
@@ -27,3 +27,8 @@ Matplotlib 3.1.1
 inputs = [1, 2, 3]
 weigths = [0.2, 0.8, -0.5]
 bias = 2
+
+# The equation is 1*0.2 + 2*0.8 + 3*-0.5 + 2, for a single neuron
+output = (inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias)
+
+print(output)
